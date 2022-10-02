e961c20c05afb16772ee718083855bc0e90a4863
Ugwu Stanley <ugwuchiagoziestanley@gmail.com>
2022-10-02 17:47:33 +0100

    checked description

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,14 +1,13 @@
-import React , {useState} from 'react'
-import styles from './Layout.module.scss'
-import Sidebar from '../components/Sidebar/Sidebar'
-import NavBar from '../components/NavBar/NavBar'
-import View from '../components/View/View'
+import React, { useState } from "react";
+import styles from "./Layout.module.scss";
+import Sidebar from "../components/Sidebar/Sidebar";
+import NavBar from "../components/NavBar/NavBar";
+import View from "../components/View/View";
 
-const Layout = ({children}) => {
-
- const [layout , setLayout] = useState("LR")
- const [downloadCount , setDownloadCount] = useState(0)
- const [code , setCode] = useState(`
+const Layout = ({ children }) => {
+  const [layout, setLayout] = useState("LR");
+  const [downloadCount, setDownloadCount] = useState(0);
+  const [code, setCode] = useState(`
  {
     "name": "json-preview",
     "version": "0.1.0",
@@ -20,12 +19,7 @@ const Layout = ({children}) => {
       "test": "react-scripts test",
       "eject": "react-scripts eject"
     },
-    "eslintConfig": {
-      "extends": [
-        "react-app",
-        "react-app/jest"
-      ]
-    },
+   
     "browserslist": {
 
       "production": [
@@ -37,25 +31,34 @@ const Layout = ({children}) => {
     }
   }
   
-  `)
-    
+  
+  `);
+
   return (
     <div className={styles.layout}>
-
-        <div className={styles.layout__navbar}>
-            <NavBar setDownloadCount={setDownloadCount} downloadCount={downloadCount} setLayout={setLayout} layout={layout} />
+      <div className={styles.layout__navbar}>
+        <NavBar
+          setDownloadCount={setDownloadCount}
+          downloadCount={downloadCount}
+          setLayout={setLayout}
+          layout={layout}
+        />
+      </div>
+      <div className={styles.layout__main}>
+        <div className={styles.layout__sidebar}>
+          <Sidebar setCode={setCode} />
         </div>
-        <div className={styles.layout__main}>
-            <div className={styles.layout__sidebar}>
-                <Sidebar setCode={setCode}  />
-            </div>
-            <div className={styles.layout__body}>
-                <View downloadCount={downloadCount} code={code} setCode={setCode} layout={layout} />
-            </div>
+        <div className={styles.layout__body}>
+          <View
+            downloadCount={downloadCount}
+            code={code}
+            setCode={setCode}
+            layout={layout}
+          />
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
